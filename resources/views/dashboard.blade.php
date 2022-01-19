6577ad07b49f8fe2ae97c213a59b7e967e89828a
marceloroot <marcelo.lima.gomes.23@gmail.com>
2022-01-19 08:26:51 -0300

    lista de arquivos

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,11 +11,66 @@
          <p>{{ $message }}</p>
         @enderror
     </h2>
-  </x-slot>
- 
 
   
+  </x-slot>
+ 
+  <div class="row mt-4 mb-4 mr-4 ml-4">
+  <div class="row ">
+    <h1 class="mb-2" style="font-size:22px; font-weight: bold;">Lista de Arquivos Necessaria (Tamanho maximo 2mb)</h1>
+    <div class="col-3">
+      <div class="list-group">
+        <a href="#" class="list-group-item list-group-item-action active">
+          Prestadores de Serviço
+        </a>
+        <a href="#" class="list-group-item list-group-item-action">Comprovante de residência</a>
+        <a href="#" class="list-group-item list-group-item-action">Contrato de Prestação de Serviço SUS</a>
+        <a href="#" class="list-group-item list-group-item-action">CNES</a>
+        <a href="#" class="list-group-item list-group-item-action">Ofício de indicação da instituição com o nome do representante</a>
+      </div>
+    </div>
 
+    <div class="col-3">
+      <div class="list-group">
+        <a href="#" class="list-group-item list-group-item-action active">
+          Entidades da Sociedade Civil 
+        </a>
+        <a href="#" class="list-group-item list-group-item-action">Comprovante de endereço</a>
+        <a href="#" class="list-group-item list-group-item-action">Estatuto</a>
+        <a href="#" class="list-group-item list-group-item-action">Ata de eleição da diretoria atual</a>
+        <a href="#" class="list-group-item list-group-item-action"> Ofício de indicação do eleitor e respectivo  suplente que representarão a entidade</a>
+      </div>
+    </div>
+
+    <div class="col-3">
+      <div class="list-group">
+        <a href="#" class="list-group-item list-group-item-action active">
+          Usuários
+        </a>
+        <a href="#" class="list-group-item list-group-item-action">CPF/RG</a>
+        <a href="#" class="list-group-item list-group-item-action"> Comprovante de endereço </a>
+        <a href="#" class="list-group-item list-group-item-action">Cartão do SUS  </a>
+      </div>
+    </div>
+
+
+
+    
+    <div class="col-3">
+      <div class="list-group">
+        <a href="#" class="list-group-item list-group-item-action active">
+          Trabalhadores da Saúde
+        </a>
+        <a href="#" class="list-group-item list-group-item-action">CPF/RG</a>
+        <a href="#" class="list-group-item list-group-item-action">Comprovante de residência</a>
+        <a href="#" class="list-group-item list-group-item-action">Registro no Conselho de Classe </a>
+      </div>
+    </div>
+
+ 
+  
+</div>
+</div>
       <div class="container">
    
          <form class="mt-5" method="POST" action="{{ route('storearquivo') }}" enctype="multipart/form-data">
